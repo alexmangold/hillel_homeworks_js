9cf4d1f9fe6bea92b0d19f4146df70ebfdb5013b
alexmangold <alex.mangold@protonmail.com>
2021-03-22 14:06:30 +0200

    h/w11 v.2

--- Homework_11/script_11.js
+++ Homework_11/script_11.js
@@ -7,7 +7,7 @@ const colors = ["cornflowerblue", "yellowgreen", "gold", "mediumorchid", "coral"
 function createSquareElements(colors) {
     const article = document.createElement("article");
     article.classList.add("square");
-    article.style.backgroundColor = colors[0];
+    article.style.backgroundColor = colors[Math.round(Math.random() * (colors.length - 1))]; // изначально сеттил color[0], но так веселее (надеюсь, не проблема :) )
 
     article.addEventListener("click", function (event) {
         if (event.target.tagName.toLowerCase() === "button") {
@@ -59,19 +59,19 @@ function createSquareElements(colors) {
 
 function createButtons() {
     const plus = document.createElement("button");
-    plus.classList.add("btn", "btn-plus");
+    plus.classList.add("btn");
     plus.setAttribute("data-buttons", "+");
     plus.innerText = "+";
-    
+
     const minus = document.createElement("button");
-    minus.classList.add("btn", "btn-minus");
+    minus.classList.add("btn");
     minus.setAttribute("data-buttons", "-");
     minus.innerText = "-";
 
     return { plus, minus };
 }
 
-for (let i = 1; i <= 5; i++) {
+for (let i = 0; i < 5; i++) {
     const square = createSquareElements(colors);
     mainContainer.appendChild(square);
 }
